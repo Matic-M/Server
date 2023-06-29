c5c714a8d3d8100754b201b8840262052a6f521b
Matic-M <qkrakenx@localhost.localdomain>
2023-06-29 10:54:04 +0200

    few fixes

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Server server = new Server(InetAddress.getByAddress(new byte[]{127,0,0,1}),8080);
 
         server.start();
-        server.send("/home/qkrakenx/Prejemi/Narcos (2015) Season 1 S01 (1080p BluRay x265 HEVC 10bit AAC 5.1 Vyndros)/Narcos.S01E10.Despegue.1080p.10bit.BluRay.AAC5.1.HEVC-Vyndros.mkv", server.getSocket());
+        server.send("/home/qkrakenx/Desktop/hereami.txt", server.getSocket());
         System.out.println(server.toString());
     }
 }
